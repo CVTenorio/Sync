b5342d10607cc668ef1f8916dcc1a6c9172fdec5
CVTenorio <202310471@fit.edu.ph>
2025-03-29 20:22:24 +0800

    tbc signup panel

--- IT0011_Finals_Sync/Testing/sync.py
+++ IT0011_Finals_Sync/Testing/sync.py
@@ -12,22 +12,56 @@ Suggestion:
 from tkinter import *
 import customtkinter
 
-window = Tk()
-window.title("Sign Up Panel")
-window.geometry("500x250")
-window.configure(bg="#817e7e")
 
-Signup_label = customtkinter.CTkLabel(
+window = Tk()
+window.title("Account_SignUp")
+window.geometry("350x350")
+window.configure(bg="#FFFFFF")
+
+
+Label_id5 = customtkinter.CTkLabel(
     master=window,
-    text="Signup",
-    font=("Times New Roman", 18),
+    text="WELCOME TO SYNC",
+    font=("Arial", 16),
     text_color="#000000",
     height=30,
-    width=95,
+    width=150,
     corner_radius=0,
-    bg_color="#817e7e",
-    fg_color="#817e7e",
-)
-
+    bg_color="#FFFFFF",
+    fg_color="#FFFFFF",
+    )
+Label_id5.place(x=100, y=130)
+Button_id7 = customtkinter.CTkButton(
+    master=window,
+    text="Sign-In",
+    font=("undefined", 14),
+    text_color="#000000",
+    hover=True,
+    hover_color="#949494",
+    height=30,
+    width=95,
+    border_width=2,
+    corner_radius=6,
+    border_color="#000000",
+    bg_color="#FFFFFF",
+    fg_color="#F0F0F0",
+    )
+Button_id7.place(x=180, y=170)
+Button_id6 = customtkinter.CTkButton(
+    master=window,
+    text="Sign-Up",
+    font=("undefined", 14),
+    text_color="#000000",
+    hover=True,
+    hover_color="#949494",
+    height=30,
+    width=95,
+    border_width=2,
+    corner_radius=6,
+    border_color="#000000",
+    bg_color="#FFFFFF",
+    fg_color="#F0F0F0",
+    )
+Button_id6.place(x=80, y=170)
 
 window.mainloop()
